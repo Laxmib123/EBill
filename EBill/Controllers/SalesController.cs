7626d71481cc639769be5e215ec00eab656a19af
Laxmib123 <bhattrailaxmi430@gmail.com>
2025-04-26 13:57:52 +0545

    UI for both Sales and purchase module completed

---
+++ EBill/Controllers/SalesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EBill.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult SalesIndex()
+        {
+            return View();
+        }
+    }
+}
